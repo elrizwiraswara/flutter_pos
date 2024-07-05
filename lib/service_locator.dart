a244a1e4f3f82e551e8cf0ee48441c56627080db
elrizwiraswara <elrizwiraswara@gmail.com>
2024-07-05 21:51:02 +0700

    feat: Added base providers

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,4 +1,11 @@
+import 'package:flutter_pos/presentation/providers/account/account_provider.dart';
+import 'package:flutter_pos/presentation/providers/auth/auth_provider.dart';
+import 'package:flutter_pos/presentation/providers/home/home_provider.dart';
+import 'package:flutter_pos/presentation/providers/main/main_provider.dart';
+import 'package:flutter_pos/presentation/providers/products/products_provider.dart';
+import 'package:flutter_pos/presentation/providers/transactions/transactions_provider.dart';
 import 'package:get_it/get_it.dart';
+import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 final GetIt sl = GetIt.instance;
@@ -16,13 +23,20 @@ void setupServiceLocator() {
   // sl.registerLazySingleton(() => UserRepositoryImpl(sl<UserDatasourceImpl>()));
 
   // // Providers
-  // sl.registerLazySingleton(() => MainProvider());
-  // sl.registerLazySingleton(() => UserListProvider(userRepository: sl<UserRepositoryImpl>()));
-  // sl.registerLazySingleton(() => UserDetailProvider(userRepository: sl<UserRepositoryImpl>()));
-  // sl.registerLazySingleton(() => UserFormProvider(userRepository: sl<UserRepositoryImpl>()));
+  sl.registerLazySingleton(() => AuthProvider());
+  sl.registerLazySingleton(() => MainProvider());
+  sl.registerLazySingleton(() => HomeProvider());
+  sl.registerLazySingleton(() => ProductsProvider());
+  sl.registerLazySingleton(() => TransactionsProvider());
+  sl.registerLazySingleton(() => AccountProvider());
 }
 
 // All providers
 final List<SingleChildWidget> providers = [
-  // ChangeNotifierProvider(create: (_) => sl<MainProvider>()),
+  ChangeNotifierProvider(create: (_) => sl<AuthProvider>()),
+  ChangeNotifierProvider(create: (_) => sl<MainProvider>()),
+  ChangeNotifierProvider(create: (_) => sl<HomeProvider>()),
+  ChangeNotifierProvider(create: (_) => sl<ProductsProvider>()),
+  ChangeNotifierProvider(create: (_) => sl<TransactionsProvider>()),
+  ChangeNotifierProvider(create: (_) => sl<AccountProvider>()),
 ];
