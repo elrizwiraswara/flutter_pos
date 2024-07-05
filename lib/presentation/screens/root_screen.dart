ee1c464934b2ff7d38772f2442894ed3b5c76cff
elrizwiraswara <elrizwiraswara@gmail.com>
2024-07-06 02:07:12 +0700

    refactor: Updated RootScreen

--- lib/presentation/screens/root_screen.dart
+++ lib/presentation/screens/root_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pos/app/assets/app_assets.dart';
+import 'package:flutter_pos/app/themes/app_sizes.dart';
+import 'package:flutter_pos/app/themes/app_theme.dart';
+import 'package:flutter_pos/presentation/widgets/app_image.dart';
 
 class RootScreen extends StatefulWidget {
   const RootScreen({super.key});
@@ -10,6 +14,34 @@ class RootScreen extends StatefulWidget {
 class _RootScreenState extends State<RootScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(child: body()),
+    );
+  }
+
+  Widget body() {
+    return Container(
+      constraints: const BoxConstraints(maxWidth: 270),
+      padding: const EdgeInsets.all(AppSizes.padding),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const AppImage(
+            image: AppAssets.welcome,
+            imgProvider: ImgProvider.assetImage,
+          ),
+          const SizedBox(height: AppSizes.padding),
+          Text(
+            'Welcome!',
+            style: AppTheme().textTheme.displaySmall?.copyWith(fontWeight: FontWeight.bold),
+          ),
+          Text(
+            'Welcome to Flutter POS app',
+            textAlign: TextAlign.center,
+            style: AppTheme().textTheme.bodyLarge,
+          ),
+        ],
+      ),
+    );
   }
 }
