4c08f79618dfafb799979203b6d46236b7f13615
elrizwiraswara <elrizwiraswara@gmail.com>
2024-07-06 22:02:30 +0700

    style: Updated widgets

--- lib/presentation/widgets/app_text_field.dart
+++ lib/presentation/widgets/app_text_field.dart
@@ -7,7 +7,7 @@ class AppTextField extends StatelessWidget {
   final bool enabled;
   final bool autofocus;
   final bool obscureText;
-  final double fontSize;
+  final double? fontSize;
   final int? minLines;
   final int maxLines;
   final int? maxLength;
@@ -30,7 +30,7 @@ class AppTextField extends StatelessWidget {
     this.enabled = true,
     this.autofocus = false,
     this.obscureText = false,
-    this.fontSize = 14,
+    this.fontSize,
     this.minLines,
     this.maxLines = 1,
     this.maxLength,
@@ -40,7 +40,7 @@ class AppTextField extends StatelessWidget {
     this.labelText,
     this.prefixIcon,
     this.suffixIcon,
-    this.contentPadding = const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+    this.contentPadding = const EdgeInsets.symmetric(horizontal: 18, vertical: 16),
     this.onChanged,
     this.onEditingComplete,
     this.inputFormatters,
@@ -50,84 +50,93 @@ class AppTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        if (labelText != null && labelText != '')
-          Padding(
-            padding: const EdgeInsets.only(bottom: 8.0),
-            child: Text(
-              labelText!,
-              style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontSize: fontSize),
+    return Opacity(
+      opacity: enabled ? 1.0 : 0.5,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (labelText != null && labelText != '')
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Text(
+                labelText!,
+                style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                      fontSize: fontSize,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+            ),
+          TextField(
+            controller: controller,
+            onChanged: onChanged,
+            onEditingComplete: onEditingComplete,
+            enabled: enabled,
+            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                  fontSize: fontSize,
+                  fontWeight: FontWeight.w600,
+                  color: Theme.of(context).colorScheme.onSurfaceVariant,
+                ),
+            cursorColor: Theme.of(context).colorScheme.primary,
+            cursorWidth: 1.5,
+            autofocus: autofocus,
+            obscureText: obscureText,
+            minLines: minLines,
+            maxLines: maxLines,
+            maxLength: maxLength,
+            maxLengthEnforcement: MaxLengthEnforcement.enforced,
+            keyboardType: keyboardType,
+            textInputAction: textInputAction,
+            inputFormatters: inputFormatters,
+            decoration: InputDecoration(
+              counterText: showCounter ? null : '',
+              isDense: true,
+              filled: true,
+              fillColor: enabled ? Theme.of(context).colorScheme.surface : Theme.of(context).colorScheme.surface,
+              prefixIcon: prefixIcon,
+              suffixIcon: suffixIcon,
+              hintText: hintText,
+              hintStyle: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    fontSize: fontSize,
+                    color: Theme.of(context).colorScheme.outlineVariant,
+                  ),
+              contentPadding: contentPadding,
+              focusedBorder: showBorder
+                  ? OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(AppSizes.radius),
+                      ),
+                      borderSide: BorderSide(
+                        width: 1,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    )
+                  : InputBorder.none,
+              enabledBorder: showBorder
+                  ? OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(AppSizes.radius),
+                      ),
+                      borderSide: BorderSide(
+                        width: 1,
+                        color: Theme.of(context).colorScheme.primaryContainer,
+                      ),
+                    )
+                  : InputBorder.none,
+              disabledBorder: showBorder
+                  ? OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(AppSizes.radius),
+                      ),
+                      borderSide: BorderSide(
+                        width: 1,
+                        color: Theme.of(context).colorScheme.surfaceDim,
+                      ),
+                    )
+                  : InputBorder.none,
             ),
           ),
-        TextField(
-          controller: controller,
-          onChanged: onChanged,
-          onEditingComplete: onEditingComplete,
-          enabled: enabled,
-          style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontSize: fontSize),
-          cursorColor: Theme.of(context).colorScheme.primary,
-          cursorWidth: 1.5,
-          autofocus: autofocus,
-          obscureText: obscureText,
-          minLines: minLines,
-          maxLines: maxLines,
-          maxLength: maxLength,
-          maxLengthEnforcement: MaxLengthEnforcement.enforced,
-          keyboardType: keyboardType,
-          textInputAction: textInputAction,
-          inputFormatters: inputFormatters,
-          decoration: InputDecoration(
-            counterText: showCounter ? null : '',
-            isDense: true,
-            filled: true,
-            fillColor:
-                enabled ? Theme.of(context).colorScheme.surfaceContainerLow : Theme.of(context).colorScheme.surfaceDim,
-            prefixIcon: prefixIcon,
-            suffixIcon: suffixIcon,
-            hintText: hintText,
-            hintStyle: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                  fontSize: fontSize,
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-            contentPadding: contentPadding,
-            focusedBorder: showBorder
-                ? OutlineInputBorder(
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(AppSizes.radius),
-                    ),
-                    borderSide: BorderSide(
-                      width: 1,
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-                  )
-                : InputBorder.none,
-            enabledBorder: showBorder
-                ? OutlineInputBorder(
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(AppSizes.radius),
-                    ),
-                    borderSide: BorderSide(
-                      width: 0.5,
-                      color: Theme.of(context).colorScheme.surfaceContainerHigh,
-                    ),
-                  )
-                : InputBorder.none,
-            disabledBorder: showBorder
-                ? OutlineInputBorder(
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(AppSizes.radius),
-                    ),
-                    borderSide: BorderSide(
-                      width: 0.5,
-                      color: Theme.of(context).colorScheme.surfaceDim,
-                    ),
-                  )
-                : InputBorder.none,
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
