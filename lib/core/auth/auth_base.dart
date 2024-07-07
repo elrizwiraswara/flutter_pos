b7d8c428803e7e7de3d6943f26edfff3235b6a0a
elrizwiraswara <elrizwiraswara@gmail.com>
2024-07-07 13:58:43 +0700

    refractor: Updated AuthBase

--- lib/core/auth/auth_base.dart
+++ lib/core/auth/auth_base.dart
@@ -2,7 +2,7 @@ import 'package:flutter_pos/core/usecase/usecase.dart';
 
 abstract class AuthBase {
   Future<bool> isAuthenticated();
-  Future<Result> getAuthData();
+  dynamic getAuthData();
   Future<Result> signIn();
   Future<bool> signOut();
 }
