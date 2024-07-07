621fb84da9b78876866e496d1a449519424c4b00
elrizwiraswara <elrizwiraswara@gmail.com>
2024-07-07 14:01:36 +0700

    refractor: Updated di & main.dart

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,7 +1,10 @@
+import 'package:flutter_pos/data/data_sources/local/app_database.dart';
+import 'package:flutter_pos/data/repositories/transaction_repository_impl.dart';
 import 'package:flutter_pos/presentation/providers/account/account_provider.dart';
 import 'package:flutter_pos/presentation/providers/auth/auth_provider.dart';
 import 'package:flutter_pos/presentation/providers/home/home_provider.dart';
 import 'package:flutter_pos/presentation/providers/main/main_provider.dart';
+import 'package:flutter_pos/presentation/providers/products/product_detail_provider.dart';
 import 'package:flutter_pos/presentation/providers/products/product_form_provider.dart';
 import 'package:flutter_pos/presentation/providers/products/products_provider.dart';
 import 'package:flutter_pos/presentation/providers/transactions/transactions_provider.dart';
@@ -12,25 +15,28 @@ import 'package:provider/single_child_widget.dart';
 final GetIt sl = GetIt.instance;
 
 // Service Locator
-void setupServiceLocator() {
-  // // Services
+void setupServiceLocator() async {
+  sl.registerSingleton<AppDatabase>(AppDatabaseConfig.database);
+
+  // Services
   // sl.registerLazySingleton(() => Client());
   // sl.registerLazySingleton(() => RestFulApi());
 
-  // // Datasources
-  // sl.registerLazySingleton(() => UserDatasourceImpl(sl<Client>(), sl<RestFulApi>()));
+  // Datasources
+  // sl.registerLazySingleton(() => TransactionDatasourceImpl(sl<Client>(), sl<RestFulApi>()));
 
-  // // Repositories
-  // sl.registerLazySingleton(() => UserRepositoryImpl(sl<UserDatasourceImpl>()));
+  // Repositories
+  sl.registerLazySingleton(() => TransactionRepositoryImpl(sl<AppDatabase>()));
 
-  // // Providers
+  // Providers
   sl.registerLazySingleton(() => AuthProvider());
   sl.registerLazySingleton(() => MainProvider());
-  sl.registerLazySingleton(() => HomeProvider());
+  sl.registerLazySingleton(() => HomeProvider(transactionRepository: sl<TransactionRepositoryImpl>()));
   sl.registerLazySingleton(() => ProductsProvider());
   sl.registerLazySingleton(() => TransactionsProvider());
   sl.registerLazySingleton(() => AccountProvider());
   sl.registerLazySingleton(() => ProductFormProvider());
+  sl.registerLazySingleton(() => ProductDetailProvider());
 }
 
 // All providers
@@ -42,4 +48,5 @@ final List<SingleChildWidget> providers = [
   ChangeNotifierProvider(create: (_) => sl<TransactionsProvider>()),
   ChangeNotifierProvider(create: (_) => sl<AccountProvider>()),
   ChangeNotifierProvider(create: (_) => sl<ProductFormProvider>()),
+  ChangeNotifierProvider(create: (_) => sl<ProductDetailProvider>()),
 ];
