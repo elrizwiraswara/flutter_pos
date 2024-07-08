bc4f82087a31089c50114721492ee51aaa1f51a4
elrizwiraswara <elrizwiraswara@gmail.com>
2024-07-08 20:00:36 +0700

    refractor: Updated domain repo and usecases

--- lib/domain/repositories/user_repository.dart
+++ lib/domain/repositories/user_repository.dart
@@ -3,7 +3,7 @@ import 'package:flutter_pos/domain/entities/user_entity.dart';
 
 abstract class UserRepository {
   Future<Result<UserEntity>> getUser(String userId);
-  Future<Result<int>> createUser(UserEntity user);
+  Future<Result<String>> createUser(UserEntity user);
   Future<void> updateUser(UserEntity user);
   Future<void> deleteUser(String userId);
 }
