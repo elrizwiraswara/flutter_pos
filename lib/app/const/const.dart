1d1ff39a6febd0f354ab29ce70b4d1b70c26ccc3
elrizwiraswara <elrizwiraswara@gmail.com>
2024-07-08 19:57:26 +0700

    feat: Added const

---
+++ lib/app/const/const.dart
@@ -0,0 +1,5 @@
+const String syncMessage = 'Data synced';
+const String unsyncMessage = 'Running data syncronizing';
+const String syncPendingMessage = 'Data will sync when internet available';
+const String onlineMessage = 'Online mode';
+const String offlineMessage = 'No internet connection, running in offline mode';
