209977632ba269596a22ffbeaaf5a86ab4d826f5
elrizwiraswara <elrizwiraswara@gmail.com>
2024-07-08 13:27:26 +0700

    refractor: Updated Transactions providers

--- lib/presentation/providers/transactions/transactions_provider.dart
+++ lib/presentation/providers/transactions/transactions_provider.dart
@@ -1,3 +1,24 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter_pos/app/services/auth/sign_in_with_google.dart';
+import 'package:flutter_pos/domain/entities/transaction_entity.dart';
+import 'package:flutter_pos/domain/repositories/transaction_repository.dart';
+import 'package:flutter_pos/domain/usecases/transaction_usecases.dart';
 
-class TransactionsProvider extends ChangeNotifier {}
+class TransactionsProvider extends ChangeNotifier {
+  final TransactionRepository transactionRepository;
+
+  TransactionsProvider({required this.transactionRepository});
+
+  List<TransactionEntity>? allTransactions;
+
+  Future<void> getAllTransactions() async {
+    var res = await GetAllTransactions(transactionRepository).call(AuthService().getAuthData()!.uid);
+
+    if (res.isSuccess) {
+      allTransactions = res.data ?? [];
+      notifyListeners();
+    } else {
+      throw res.error?.error ?? 'Failed to load data';
+    }
+  }
+}
