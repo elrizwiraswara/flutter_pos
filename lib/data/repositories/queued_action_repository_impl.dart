ee7df3b73b9b36b0b571035b19e791d2f38dda63
elrizwiraswara <elrizwiraswara@gmail.com>
2024-07-09 13:36:22 +0700

    refractor: Updated repositories, added queued action repository

---
+++ lib/data/repositories/queued_action_repository_impl.dart
@@ -0,0 +1,133 @@
+import 'dart:convert';
+
+import 'package:flutter_pos/app/services/connectivity/connectivity_service.dart';
+import 'package:flutter_pos/app/utilities/console_log.dart';
+import 'package:flutter_pos/core/usecase/usecase.dart';
+import 'package:flutter_pos/data/data_sources/local/queued_action_local_datasource_impl.dart';
+import 'package:flutter_pos/data/models/product_model.dart';
+import 'package:flutter_pos/data/models/queued_action_model.dart';
+import 'package:flutter_pos/data/models/transaction_model.dart';
+import 'package:flutter_pos/data/models/user_model.dart';
+import 'package:flutter_pos/data/repositories/product_repository_impl.dart';
+import 'package:flutter_pos/data/repositories/transaction_repository_impl.dart';
+import 'package:flutter_pos/data/repositories/user_repository_impl.dart';
+import 'package:flutter_pos/domain/entities/product_entity.dart';
+import 'package:flutter_pos/domain/entities/queued_action_entity.dart';
+import 'package:flutter_pos/domain/entities/transaction_entity.dart';
+import 'package:flutter_pos/domain/entities/user_entity.dart';
+import 'package:flutter_pos/domain/repositories/queued_action_repository.dart';
+import 'package:flutter_pos/service_locator.dart';
+
+class QueuedActionRepositoryImpl extends QueuedActionRepository {
+  final QueuedActionLocalDatasourceImpl queuedActionLocalDatasourceImpl;
+
+  QueuedActionRepositoryImpl({
+    required this.queuedActionLocalDatasourceImpl,
+  });
+  @override
+  Future<Result<List<QueuedActionEntity>>> getAllQueuedAction() async {
+    var res = await queuedActionLocalDatasourceImpl.getAllUserQueuedAction();
+    return Result.success(res.map((e) => e.toEntity()).toList());
+  }
+
+  @override
+  Future<Result<List<bool>>> executeAllQueuedActions(List<QueuedActionEntity> queues) async {
+    if (queues.isEmpty) return Result.success([]);
+
+    if (!ConnectivityService.isConnected) return Result.success([]);
+
+    List<bool> result = [];
+
+    for (var queue in queues) {
+      var res = await executeQueuedAction(queue);
+
+      if (res.isSuccess) {
+        result.add(res.data ?? false);
+      } else {
+        result.add(false);
+      }
+    }
+
+    return Result.success(result);
+  }
+
+  @override
+  Future<Result<bool>> executeQueuedAction(QueuedActionEntity queue) async {
+    try {
+      cl("[executeQueuedAction].queue = ${QueuedActionModel.fromEntity(queue).toJson()}");
+
+      var res = await _functionSelector(queue);
+
+      if (res.isSuccess) {
+        // Delete executed queue from db
+        await queuedActionLocalDatasourceImpl.deleteQueuedAction(queue.id!);
+        return Result.success(true);
+      } else {
+        return Result.error(null);
+      }
+    } catch (e) {
+      return Result.error(null);
+    }
+  }
+
+  Future<Result> _functionSelector(QueuedActionEntity queue) async {
+    if (queue.repository == 'UserRepositoryImpl') {
+      if (queue.method == 'createUser') {
+        UserEntity param = UserModel.fromJson(jsonDecode(queue.param)).toEntity();
+        return sl<UserRepositoryImpl>().createUser(param);
+      }
+
+      if (queue.method == 'deleteUser') {
+        var param = queue.param;
+        await sl<UserRepositoryImpl>().deleteUser(param);
+        return Result.success(null);
+      }
+
+      if (queue.method == 'updateUser') {
+        UserEntity param = UserModel.fromJson(jsonDecode(queue.param)).toEntity();
+        await sl<UserRepositoryImpl>().updateUser(param);
+        return Result.success(null);
+      }
+    }
+
+    if (queue.repository == 'TransactionRepositoryImpl') {
+      if (queue.method == 'createTransaction') {
+        TransactionEntity param = TransactionModel.fromJson(jsonDecode(queue.param)).toEntity();
+        return sl<TransactionRepositoryImpl>().createTransaction(param);
+      }
+
+      if (queue.method == 'deleteTransaction') {
+        var param = int.parse(queue.param);
+        await sl<TransactionRepositoryImpl>().deleteTransaction(param);
+        return Result.success(null);
+      }
+
+      if (queue.method == 'updateTransaction') {
+        TransactionEntity param = TransactionModel.fromJson(jsonDecode(queue.param)).toEntity();
+        await sl<TransactionRepositoryImpl>().updateTransaction(param);
+        return Result.success(null);
+      }
+    }
+
+    if (queue.repository == 'ProductRepositoryImpl') {
+      if (queue.method == 'createProduct') {
+        ProductEntity param = ProductModel.fromJson(jsonDecode(queue.param)).toEntity();
+        return sl<ProductRepositoryImpl>().createProduct(param);
+      }
+
+      if (queue.method == 'deleteProduct') {
+        var param = int.parse(queue.param);
+        await sl<ProductRepositoryImpl>().deleteProduct(param);
+        return Result.success(null);
+      }
+
+      if (queue.method == 'updateProduct') {
+        ProductEntity param = ProductModel.fromJson(jsonDecode(queue.param)).toEntity();
+        await sl<ProductRepositoryImpl>().updateProduct(param);
+        return Result.success(null);
+      }
+    }
+
+    return Future.value(Result.success(null));
+  }
+}
--- lib/data/repositories/transaction_repository_impl.dart
+++ lib/data/repositories/transaction_repository_impl.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+
+import 'package:flutter_pos/app/const/const.dart';
+import 'package:flutter_pos/app/services/connectivity/connectivity_service.dart';
 import 'package:flutter_pos/core/usecase/usecase.dart';
+import 'package:flutter_pos/data/data_sources/local/queued_action_local_datasource_impl.dart';
 import 'package:flutter_pos/data/data_sources/local/transaction_local_datasource_impl.dart';
 import 'package:flutter_pos/data/data_sources/remote/transaction_remote_datasource_impl.dart';
+import 'package:flutter_pos/data/models/queued_action_model.dart';
 import 'package:flutter_pos/data/models/transaction_model.dart';
 import 'package:flutter_pos/domain/entities/transaction_entity.dart';
 import 'package:flutter_pos/domain/repositories/transaction_repository.dart';
@@ -8,34 +14,178 @@ import 'package:flutter_pos/domain/repositories/transaction_repository.dart';
 class TransactionRepositoryImpl extends TransactionRepository {
   final TransactionLocalDatasourceImpl transactionLocalDatasource;
   final TransactionRemoteDatasourceImpl transactionRemoteDatasource;
+  final QueuedActionLocalDatasourceImpl queuedActionLocalDatasourceImpl;
 
-  TransactionRepositoryImpl({required this.transactionLocalDatasource, required this.transactionRemoteDatasource});
+  TransactionRepositoryImpl({
+    required this.transactionLocalDatasource,
+    required this.transactionRemoteDatasource,
+    required this.queuedActionLocalDatasourceImpl,
+  });
 
   @override
   Future<Result<List<TransactionEntity>>> getAllUserTransactions(String userId) async {
-    var res = await transactionLocalDatasource.getAllUserTransactions(userId);
-    return Result.success(res.map((e) => e.toEntity()).toList());
+    var local = await transactionLocalDatasource.getAllUserTransactions(userId);
+
+    if (ConnectivityService.isConnected) {
+      var remote = await transactionRemoteDatasource.getAllUserTransactions(userId);
+
+      if (remote.isEmpty && local.isNotEmpty) {
+        for (var data in local) {
+          // Store local data to remote db
+          await transactionRemoteDatasource.createTransaction(data);
+        }
+
+        // Return local data
+        return Result.success(local.map((e) => e.toEntity()).toList());
+      }
+
+      if (remote.isNotEmpty && local.isEmpty) {
+        for (var data in remote) {
+          // Store remote data to local db
+          await transactionLocalDatasource.createTransaction(data);
+        }
+
+        // Return remote data
+        return Result.success(remote.map((e) => e.toEntity()).toList());
+      }
+
+      if (remote.isNotEmpty && local.isNotEmpty) {
+        List<bool> isRemoteHasNewerData = [];
+
+        for (var localData in local) {
+          var matchRemoteData = remote.where((remoteProduct) => remoteProduct.id == localData.id).firstOrNull;
+
+          if (matchRemoteData != null) {
+            var updatedAtLocal = DateTime.tryParse(localData.updatedAt ?? DateTime.now().toIso8601String());
+            var updatedAtRemote = DateTime.tryParse(matchRemoteData.updatedAt ?? DateTime.now().toIso8601String());
+            var differenceInMinutes = updatedAtRemote?.difference(updatedAtLocal!).inMinutes ?? 0;
+            var isRemoteNewer = differenceInMinutes > MIN_SYNC_INTERVAL_TOLERANCE_FOR_LESS_CRITICAL_IN_MINUTES;
+
+            if (isRemoteNewer) {
+              isRemoteHasNewerData.add(true);
+              // Save remote data to local db
+              await transactionLocalDatasource.updateTransaction(matchRemoteData);
+            } else {
+              // Update remote with local data
+              await transactionRemoteDatasource.updateTransaction(localData);
+            }
+          } else {
+            // No matching remote product, create it
+            await transactionRemoteDatasource.createTransaction(localData);
+          }
+        }
+
+        if (isRemoteHasNewerData.contains(true)) {
+          // Return remote data
+          return Result.success(remote.map((e) => e.toEntity()).toList());
+        } else {
+          // Return local data
+          return Result.success(local.map((e) => e.toEntity()).toList());
+        }
+      }
+    }
+
+    return Result.success(local.map((e) => e.toEntity()).toList());
   }
 
   @override
   Future<Result<TransactionEntity>> getTransaction(int transactionId) async {
-    var res = await transactionLocalDatasource.getTransaction(transactionId);
-    return Result.success(res?.toEntity());
+    var local = await transactionLocalDatasource.getTransaction(transactionId);
+
+    if (ConnectivityService.isConnected) {
+      var remote = await transactionRemoteDatasource.getTransaction(transactionId);
+
+      if (remote == null && local != null) {
+        // Store local data to remote db
+        await transactionRemoteDatasource.createTransaction(local);
+        // Return local data
+        return Result.success(local.toEntity());
+      }
+
+      if (remote != null && local == null) {
+        // Store remote data to local db
+        await transactionLocalDatasource.createTransaction(remote);
+        // Return remote data
+        return Result.success(remote.toEntity());
+      }
+
+      if (remote != null && local != null) {
+        var updatedAtLocal = DateTime.tryParse(local.updatedAt ?? DateTime.now().toIso8601String());
+        var updatedAtRemote = DateTime.tryParse(remote.updatedAt ?? DateTime.now().toIso8601String());
+        var differenceInMinutes = updatedAtRemote?.difference(updatedAtLocal!).inMinutes ?? 0;
+        var isRemoteNewer = differenceInMinutes > MIN_SYNC_INTERVAL_TOLERANCE_FOR_LESS_CRITICAL_IN_MINUTES;
+
+        // Compare local & remote data updatedAt difference
+        if (isRemoteNewer) {
+          // Save remote data to local db
+          await transactionLocalDatasource.updateTransaction(remote);
+          // Return remote data
+          return Result.success(remote.toEntity());
+        } else {
+          // Store local data to remote db
+          await transactionRemoteDatasource.updateTransaction(local);
+          // Return local data
+          return Result.success(local.toEntity());
+        }
+      }
+    }
+
+    return Result.success(local?.toEntity());
   }
 
   @override
   Future<Result<int>> createTransaction(TransactionEntity transaction) async {
     var id = await transactionLocalDatasource.createTransaction(TransactionModel.fromEntity(transaction));
+
+    if (ConnectivityService.isConnected) {
+      await transactionRemoteDatasource.createTransaction(TransactionModel.fromEntity(transaction)..id = id);
+    } else {
+      await queuedActionLocalDatasourceImpl.createQueuedAction(
+        QueuedActionModel(
+          repository: 'TransactionRepositoryImpl',
+          method: 'createTransaction',
+          param: jsonEncode((TransactionModel.fromEntity(transaction)..id = id).toJson()),
+          isCritical: true,
+        ),
+      );
+    }
+
     return Result.success(id);
   }
 
   @override
   Future<void> deleteTransaction(int transactionId) async {
-    return await transactionLocalDatasource.deleteTransaction(transactionId);
+    await transactionLocalDatasource.deleteTransaction(transactionId);
+
+    if (ConnectivityService.isConnected) {
+      await transactionRemoteDatasource.deleteTransaction(transactionId);
+    } else {
+      await queuedActionLocalDatasourceImpl.createQueuedAction(
+        QueuedActionModel(
+          repository: 'TransactionRepositoryImpl',
+          method: 'deleteTransaction',
+          param: transactionId.toString(),
+          isCritical: true,
+        ),
+      );
+    }
   }
 
   @override
   Future<void> updateTransaction(TransactionEntity transaction) async {
-    return await transactionLocalDatasource.updateTransaction(TransactionModel.fromEntity(transaction));
+    await transactionLocalDatasource.updateTransaction(TransactionModel.fromEntity(transaction));
+
+    if (ConnectivityService.isConnected) {
+      await transactionRemoteDatasource.updateTransaction(TransactionModel.fromEntity(transaction));
+    } else {
+      await queuedActionLocalDatasourceImpl.createQueuedAction(
+        QueuedActionModel(
+          repository: 'TransactionRepositoryImpl',
+          method: 'updateTransaction',
+          param: jsonEncode(TransactionModel.fromEntity(transaction).toJson()),
+          isCritical: true,
+        ),
+      );
+    }
   }
 }
