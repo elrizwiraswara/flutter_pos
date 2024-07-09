892d3abfb981005c0da0b962478357b134946491
elrizwiraswara <elrizwiraswara@gmail.com>
2024-07-09 13:38:23 +0700

    refractor: Updated main provider, added data syncronizing related features

--- lib/presentation/providers/main/main_provider.dart
+++ lib/presentation/providers/main/main_provider.dart
@@ -3,92 +3,139 @@ import 'package:flutter_pos/app/const/const.dart';
 import 'package:flutter_pos/app/routes/app_routes.dart';
 import 'package:flutter_pos/app/services/auth/auth_service.dart';
 import 'package:flutter_pos/app/services/connectivity/connectivity_service.dart';
+import 'package:flutter_pos/core/usecase/usecase.dart';
+import 'package:flutter_pos/domain/entities/queued_action_entity.dart';
 import 'package:flutter_pos/domain/entities/user_entity.dart';
 import 'package:flutter_pos/domain/repositories/product_repository.dart';
+import 'package:flutter_pos/domain/repositories/queued_action_repository.dart';
 import 'package:flutter_pos/domain/repositories/transaction_repository.dart';
 import 'package:flutter_pos/domain/repositories/user_repository.dart';
+import 'package:flutter_pos/domain/usecases/product_usecases.dart';
+import 'package:flutter_pos/domain/usecases/ququed_action_usecases.dart';
+import 'package:flutter_pos/domain/usecases/transaction_usecases.dart';
 import 'package:flutter_pos/domain/usecases/user_usecases.dart';
+import 'package:flutter_pos/presentation/providers/products/products_provider.dart';
+import 'package:flutter_pos/service_locator.dart';
 
 class MainProvider extends ChangeNotifier {
   final UserRepository userRepository;
   final ProductRepository productRepository;
   final TransactionRepository transactionRepository;
+  final QueuedActionRepository queuedActionRepository;
 
   MainProvider({
     required this.transactionRepository,
     required this.userRepository,
     required this.productRepository,
+    required this.queuedActionRepository,
   });
 
   bool isLoaded = false;
   bool isHasInternet = false;
   bool isDataSynced = false;
+  bool isSyncronizing = false;
 
   UserEntity? user;
 
   Future<void> initMainProvider() async {
+    isLoaded = false;
+    notifyListeners();
+
     await ConnectivityService.initNetworkChecker(onHasInternet: onHasInternet);
 
-    await checkAndSaveUserData();
-    await checkAndSaveUserProducts();
-    await checkAndSaveUserTransactions();
+    await checkAndSaveAllData();
 
     isLoaded = true;
     notifyListeners();
   }
 
-  Future<void> checkAndSaveUserData() async {
-    var auth = AuthService().getAuthData();
+  Future<void> checkAndSyncAllData() async {
+    final context = AppRoutes.rootNavigatorKey.currentState!.context;
+    final theme = Theme.of(context);
+    final messenger = ScaffoldMessenger.of(context);
 
-    if (auth == null) return;
+    try {
+      messenger.hideCurrentSnackBar();
+      messenger.showSnackBar(const SnackBar(content: Text(SYNCRONIZING_MESSAGE)));
 
-    var res = await GetUserUsecase(userRepository).call(auth.uid);
+      isSyncronizing = true;
+      notifyListeners();
 
-    if (res.isSuccess && res.data == null) {
-      var userEntity = UserEntity(
-        id: auth.uid,
-        name: auth.displayName,
-        email: auth.email,
-        phone: auth.phoneNumber,
-        imageUrl: auth.photoURL,
+      await checkAndSaveAllData();
+
+      // Check and execute all queued actions
+      int queueExecutedCount = await checkAndExecutePendingQueue();
+
+      messenger.hideCurrentSnackBar();
+      messenger.showSnackBar(
+        SnackBar(
+          content: Text("$SYNCED_MESSAGE! ${queueExecutedCount > 0 ? "$queueExecutedCount queues executed" : ""}"),
+        ),
       );
 
-      await CreateUserUsecase(userRepository).call(userEntity);
+      isSyncronizing = false;
+      isDataSynced = true;
+      notifyListeners();
+    } catch (e) {
+      isSyncronizing = false;
+      notifyListeners();
 
-      user = userEntity;
-      notifyListeners();
-    } else {
-      user = res.data;
-      notifyListeners();
+      messenger.hideCurrentSnackBar();
+      messenger.showSnackBar(
+        SnackBar(
+          content: Text('Failed to sync data\n\n${e.toString()}'),
+          backgroundColor: theme.colorScheme.error,
+        ),
+      );
     }
   }
 
-  Future<void> checkAndSaveUserProducts() async {}
+  Future<void> checkAndSaveAllData() async {
+    var auth = AuthService().getAuthData();
+    if (auth == null) return;
 
-  Future<void> checkAndSaveUserTransactions() async {}
+    // Run multiple futures simultaneusly
+    // Because each repository has beed added data checker method
+    // The local db will automatically sync with cloud db or vice versa
+    var res = await Future.wait([
+      GetUserUsecase(userRepository).call(auth.uid),
+      GetAllProductsUsecase(productRepository).call(auth.uid),
+      GetAllTransactionsUsecase(transactionRepository).call(auth.uid),
+    ]);
+
+    // Set and notify user state
+    if (res.first.isSuccess) {
+      user = res.first.data as UserEntity?;
+      notifyListeners();
+    }
+
+    // Refresh products list
+    sl<ProductsProvider>().getAllProducts();
+  }
+
+  Future<int> checkAndExecutePendingQueue() async {
+    var queuedActions = await getQueuedActions();
+
+    if (queuedActions.isNotEmpty) {
+      var res = await ExecuteAllQueuedActionUsecase(queuedActionRepository).call(queuedActions);
+
+      int executedCount = res.data?.where((e) => e).length ?? 0;
+      return executedCount;
+    }
+
+    return 0;
+  }
+
+  Future<List<QueuedActionEntity>> getQueuedActions() async {
+    var res = await GetAllQueuedActionUsecase(queuedActionRepository).call(NoParams());
+    return res.data ?? [];
+  }
 
   Future<void> onHasInternet(bool value) async {
     isHasInternet = value;
+    isDataSynced = (await getQueuedActions()).isNotEmpty;
     notifyListeners();
 
-    var message = '';
-
-    if (isDataSynced) {
-      message = syncMessage;
-    }
-
-    if (isHasInternet && !isDataSynced) {
-      message = unsyncMessage;
-    }
-
-    if (!isHasInternet && !isDataSynced) {
-      message = syncPendingMessage;
-    }
-
-    if (message.isEmpty && AppRoutes.rootNavigatorKey.currentState == null) return;
-
-    ScaffoldMessenger.of(AppRoutes.rootNavigatorKey.currentState!.context).showSnackBar(
-      SnackBar(content: Text(message)),
-    );
+    if (isHasInternet) checkAndSyncAllData();
   }
 }
