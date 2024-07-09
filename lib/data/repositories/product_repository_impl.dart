aedc57c660de0e148d503e2c68de58d2f6521a9d
elrizwiraswara <elrizwiraswara@gmail.com>
2024-07-09 13:36:22 +0700

    refractor: Updated repositories, added queued action repository

--- lib/data/repositories/product_repository_impl.dart
+++ lib/data/repositories/product_repository_impl.dart
@@ -1,41 +1,189 @@
+import 'dart:convert';
+
+import 'package:flutter_pos/app/const/const.dart';
+import 'package:flutter_pos/app/services/connectivity/connectivity_service.dart';
 import 'package:flutter_pos/core/usecase/usecase.dart';
 import 'package:flutter_pos/data/data_sources/local/product_local_datasource_impl.dart';
+import 'package:flutter_pos/data/data_sources/local/queued_action_local_datasource_impl.dart';
 import 'package:flutter_pos/data/data_sources/remote/product_remote_datasource_impl.dart';
 import 'package:flutter_pos/data/models/product_model.dart';
+import 'package:flutter_pos/data/models/queued_action_model.dart';
 import 'package:flutter_pos/domain/entities/product_entity.dart';
 import 'package:flutter_pos/domain/repositories/product_repository.dart';
 
 class ProductRepositoryImpl extends ProductRepository {
   final ProductLocalDatasourceImpl productLocalDatasource;
   final ProductRemoteDatasourceImpl productRemoteDatasource;
+  final QueuedActionLocalDatasourceImpl queuedActionLocalDatasourceImpl;
 
-  ProductRepositoryImpl({required this.productLocalDatasource, required this.productRemoteDatasource});
+  ProductRepositoryImpl({
+    required this.productLocalDatasource,
+    required this.productRemoteDatasource,
+    required this.queuedActionLocalDatasourceImpl,
+  });
 
   @override
   Future<Result<List<ProductEntity>>> getAllUserProducts(String userId) async {
-    var res = await productRemoteDatasource.getAllUserProduct(userId);
-    return Result.success(res.map((e) => e.toEntity()).toList());
+    var local = await productLocalDatasource.getAllUserProduct(userId);
+
+    if (ConnectivityService.isConnected) {
+      var remote = await productRemoteDatasource.getAllUserProduct(userId);
+
+      if (remote.isEmpty && local.isNotEmpty) {
+        for (var data in local) {
+          // Store local data to remote db
+          await productRemoteDatasource.createProduct(data);
+        }
+        // Return local data
+        return Result.success(local.map((e) => e.toEntity()).toList());
+      }
+
+      if (remote.isNotEmpty && local.isEmpty) {
+        for (var data in remote) {
+          // Store remote data to local db
+          await productLocalDatasource.createProduct(data);
+        }
+        // Return remote data
+        return Result.success(remote.map((e) => e.toEntity()).toList());
+      }
+
+      if (remote.isNotEmpty && local.isNotEmpty) {
+        List<bool> isRemoteHasNewerData = [];
+
+        for (var localData in local) {
+          var matchRemoteData = remote.where((remoteData) => remoteData.id == localData.id).firstOrNull;
+
+          if (matchRemoteData != null) {
+            var updatedAtLocal = DateTime.tryParse(localData.updatedAt ?? DateTime.now().toIso8601String());
+            var updatedAtRemote = DateTime.tryParse(matchRemoteData.updatedAt ?? DateTime.now().toIso8601String());
+            var differenceInMinutes = updatedAtRemote?.difference(updatedAtLocal!).inMinutes ?? 0;
+            var isRemoteNewer = differenceInMinutes > MIN_SYNC_INTERVAL_TOLERANCE_FOR_LESS_CRITICAL_IN_MINUTES;
+
+            if (isRemoteNewer) {
+              isRemoteHasNewerData.add(true);
+              // Save remote data to local db
+              await productLocalDatasource.updateProduct(matchRemoteData);
+            } else {
+              // Update remote with local data
+              await productRemoteDatasource.updateProduct(localData);
+            }
+          } else {
+            // No matching remote product, create it
+            await productRemoteDatasource.createProduct(localData);
+          }
+        }
+
+        if (isRemoteHasNewerData.contains(true)) {
+          // Return remote data
+          return Result.success(remote.map((e) => e.toEntity()).toList());
+        } else {
+          // Return local data
+          return Result.success(local.map((e) => e.toEntity()).toList());
+        }
+      }
+    }
+
+    return Result.success(local.map((e) => e.toEntity()).toList());
   }
 
   @override
   Future<Result<ProductEntity>> getProduct(int productId) async {
-    var res = await productRemoteDatasource.getProduct(productId);
-    return Result.success(res?.toEntity());
+    var local = await productLocalDatasource.getProduct(productId);
+
+    if (ConnectivityService.isConnected) {
+      var remote = await productRemoteDatasource.getProduct(productId);
+
+      if (remote == null && local != null) {
+        // Store local data to remote db
+        await productRemoteDatasource.createProduct(local);
+        // Return local data
+        return Result.success(local.toEntity());
+      }
+
+      if (remote != null && local == null) {
+        // Store remote data to local db
+        await productLocalDatasource.createProduct(remote);
+        // Return remote data
+        return Result.success(remote.toEntity());
+      }
+
+      if (remote != null && local != null) {
+        var updatedAtLocal = DateTime.tryParse(local.updatedAt ?? DateTime.now().toIso8601String());
+        var updatedAtRemote = DateTime.tryParse(remote.updatedAt ?? DateTime.now().toIso8601String());
+        var differenceInMinutes = updatedAtRemote?.difference(updatedAtLocal!).inMinutes ?? 0;
+        var isRemoteNewer = differenceInMinutes > MIN_SYNC_INTERVAL_TOLERANCE_FOR_LESS_CRITICAL_IN_MINUTES;
+
+        // Compare local & remote data updatedAt difference
+        if (isRemoteNewer) {
+          // Save remote data to local db
+          await productLocalDatasource.updateProduct(remote);
+          // Return remote data
+          return Result.success(remote.toEntity());
+        } else {
+          // Store local data to remote db
+          await productRemoteDatasource.updateProduct(local);
+          // Return local data
+          return Result.success(local.toEntity());
+        }
+      }
+    }
+
+    return Result.success(local?.toEntity());
   }
 
   @override
   Future<Result<int>> createProduct(ProductEntity product) async {
-    var productId = await productRemoteDatasource.createProduct(ProductModel.fromEntity(product));
+    var productId = await productLocalDatasource.createProduct(ProductModel.fromEntity(product));
+
+    if (ConnectivityService.isConnected) {
+      await productRemoteDatasource.createProduct(ProductModel.fromEntity(product)..id = productId);
+    } else {
+      await queuedActionLocalDatasourceImpl.createQueuedAction(
+        QueuedActionModel(
+          repository: 'ProductRepositoryImpl',
+          method: 'createProduct',
+          param: jsonEncode((ProductModel.fromEntity(product)..id = productId).toJson()),
+          isCritical: true,
+        ),
+      );
+    }
+
     return Result.success(productId);
   }
 
   @override
   Future<void> deleteProduct(int productId) async {
-    return await productRemoteDatasource.deleteProduct(productId);
+    await productLocalDatasource.deleteProduct(productId);
+
+    if (ConnectivityService.isConnected) {
+      await productRemoteDatasource.deleteProduct(productId);
+    } else {
+      await queuedActionLocalDatasourceImpl.createQueuedAction(
+        QueuedActionModel(
+          repository: 'ProductRepositoryImpl',
+          method: 'deleteProduct',
+          param: productId.toString(),
+          isCritical: true,
+        ),
+      );
+    }
   }
 
   @override
   Future<void> updateProduct(ProductEntity product) async {
-    return await productRemoteDatasource.updateProduct(ProductModel.fromEntity(product));
+    await productLocalDatasource.updateProduct(ProductModel.fromEntity(product));
+
+    if (ConnectivityService.isConnected) {
+      await productRemoteDatasource.updateProduct(ProductModel.fromEntity(product));
+    } else {
+      await queuedActionLocalDatasourceImpl.createQueuedAction(
+        QueuedActionModel(
+          repository: 'ProductRepositoryImpl',
+          method: 'updateProduct',
+          param: jsonEncode(ProductModel.fromEntity(product).toJson()),
+          isCritical: true,
+        ),
+      );
+    }
   }
 }
--- lib/data/repositories/user_repository_impl.dart
+++ lib/data/repositories/user_repository_impl.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+
+import 'package:flutter_pos/app/const/const.dart';
+import 'package:flutter_pos/app/services/connectivity/connectivity_service.dart';
 import 'package:flutter_pos/core/usecase/usecase.dart';
+import 'package:flutter_pos/data/data_sources/local/queued_action_local_datasource_impl.dart';
 import 'package:flutter_pos/data/data_sources/local/user_local_datasource_impl.dart';
 import 'package:flutter_pos/data/data_sources/remote/user_remote_datasource_impl.dart';
+import 'package:flutter_pos/data/models/queued_action_model.dart';
 import 'package:flutter_pos/data/models/user_model.dart';
 import 'package:flutter_pos/domain/entities/user_entity.dart';
 import 'package:flutter_pos/domain/repositories/user_repository.dart';
@@ -8,28 +14,112 @@ import 'package:flutter_pos/domain/repositories/user_repository.dart';
 class UserRepositoryImpl extends UserRepository {
   final UserLocalDatasourceImpl userLocalDatasource;
   final UserRemoteDatasourceImpl userRemoteDatasource;
+  final QueuedActionLocalDatasourceImpl queuedActionLocalDatasourceImpl;
 
-  UserRepositoryImpl({required this.userLocalDatasource, required this.userRemoteDatasource});
+  UserRepositoryImpl({
+    required this.userLocalDatasource,
+    required this.userRemoteDatasource,
+    required this.queuedActionLocalDatasourceImpl,
+  });
 
   @override
   Future<Result<UserEntity>> getUser(String userId) async {
-    var res = await userLocalDatasource.getUser(userId);
-    return Result.success(res?.toEntity());
+    var local = await userLocalDatasource.getUser(userId);
+
+    if (ConnectivityService.isConnected) {
+      var remote = await userRemoteDatasource.getUser(userId);
+
+      if (remote == null && local != null) {
+        // Store local data to remote db
+        await userRemoteDatasource.createUser(local);
+        // Return local data
+        return Result.success(local.toEntity());
+      }
+
+      if (remote != null && local == null) {
+        // Store remote data to local db
+        await userLocalDatasource.createUser(remote);
+        // Return remote data
+        return Result.success(remote.toEntity());
+      }
+
+      if (remote != null && local != null) {
+        var updatedAtLocal = DateTime.tryParse(local.updatedAt ?? DateTime.now().toIso8601String());
+        var updatedAtRemote = DateTime.tryParse(remote.updatedAt ?? DateTime.now().toIso8601String());
+        var differenceInMinutes = updatedAtRemote?.difference(updatedAtLocal!).inMinutes ?? 0;
+        var isRemoteNewer = differenceInMinutes > MIN_SYNC_INTERVAL_TOLERANCE_FOR_LESS_CRITICAL_IN_MINUTES;
+
+        // Compare local & remote data updatedAt difference
+        if (isRemoteNewer) {
+          // Save remote data to local db
+          await userLocalDatasource.updateUser(remote);
+          // Return remote data
+          return Result.success(remote.toEntity());
+        } else {
+          // Store local data to remote db
+          await userRemoteDatasource.updateUser(local);
+          // Return local data
+          return Result.success(local.toEntity());
+        }
+      }
+    }
+
+    return Result.success(local?.toEntity());
   }
 
   @override
   Future<Result<String>> createUser(UserEntity user) async {
     var id = await userLocalDatasource.createUser(UserModel.fromEntity(user));
+
+    if (ConnectivityService.isConnected) {
+      await userRemoteDatasource.createUser(UserModel.fromEntity(user)..id = id);
+    } else {
+      await queuedActionLocalDatasourceImpl.createQueuedAction(
+        QueuedActionModel(
+          repository: 'UserRepositoryImpl',
+          method: 'createUser',
+          param: jsonEncode((UserModel.fromEntity(user)..id = id).toJson()),
+          isCritical: false,
+        ),
+      );
+    }
+
     return Result.success(id);
   }
 
   @override
   Future<void> deleteUser(String userId) async {
-    return await userLocalDatasource.deleteUser(userId);
+    await userLocalDatasource.deleteUser(userId);
+
+    if (ConnectivityService.isConnected) {
+      await userRemoteDatasource.deleteUser(userId);
+    } else {
+      await queuedActionLocalDatasourceImpl.createQueuedAction(
+        QueuedActionModel(
+          repository: 'UserRepositoryImpl',
+          method: 'deleteUser',
+          param: userId,
+          isCritical: false,
+        ),
+      );
+    }
   }
 
   @override
   Future<void> updateUser(UserEntity user) async {
-    return await userLocalDatasource.updateUser(UserModel.fromEntity(user));
+    await userLocalDatasource.updateUser(UserModel.fromEntity(user));
+
+    if (ConnectivityService.isConnected) {
+      await userRemoteDatasource.updateUser(UserModel.fromEntity(user));
+    } else {
+      await queuedActionLocalDatasourceImpl.createQueuedAction(
+        QueuedActionModel(
+          repository: 'UserRepositoryImpl',
+          method: 'updateUser',
+          param: jsonEncode(UserModel.fromEntity(user).toJson()),
+          isCritical: false,
+        ),
+      );
+    }
   }
 }
