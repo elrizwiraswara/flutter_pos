4c64e1f640bf337ca5d01f111341dffb9c0ea854
elrizwiraswara <elrizwiraswara@gmail.com>
2024-07-09 13:41:54 +0700

    feat: Added theme provider

---
+++ lib/presentation/providers/theme/theme_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pos/app/themes/app_theme.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeData theme = AppTheme().init();
+
+  void changeBrightness(bool isLight) {
+    var brightness = isLight ? Brightness.light : Brightness.dark;
+    theme = AppTheme().init(brightness: brightness);
+    notifyListeners();
+  }
+
+  bool isLight() => theme.brightness == Brightness.light;
+}
