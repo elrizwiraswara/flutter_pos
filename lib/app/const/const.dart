7e30b4c64fedcf83a1bf27e7d40cd75f21c7ce4e
elrizwiraswara <elrizwiraswara@gmail.com>
2024-07-09 13:34:10 +0700

    refractor: Updated const, removed dummy

--- lib/app/const/const.dart
+++ lib/app/const/const.dart
@@ -1,5 +1,10 @@
-const String syncMessage = 'Data synced';
-const String unsyncMessage = 'Running data syncronizing';
-const String syncPendingMessage = 'Data will sync when internet available';
-const String onlineMessage = 'Online mode';
-const String offlineMessage = 'No internet connection, running in offline mode';
+// ignore_for_file: constant_identifier_names
+
+const String SYNCED_MESSAGE = 'Data synced';
+const String SYNCRONIZING_MESSAGE = 'Running data syncronizing';
+const String SYNC_PENDING_MESSAGE = 'Data will sync when internet available';
+const String ONLINE_MESSAGE = 'Online mode';
+const String OFFLINE_MESSAGE = 'No internet connection, running in offline mode';
+
+const int MIN_SYNC_INTERVAL_TOLERANCE_FOR_CRITICAL_IN_MINUTES = 5;
+const int MIN_SYNC_INTERVAL_TOLERANCE_FOR_LESS_CRITICAL_IN_MINUTES = 100;
