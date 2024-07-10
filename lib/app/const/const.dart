0e8df6656286c0f6949296852a10b48922645270
elrizwiraswara <elrizwiraswara@gmail.com>
2024-07-10 07:53:42 +0700

    refractor: Added const string for the first time without internet error message

--- lib/app/const/const.dart
+++ lib/app/const/const.dart
@@ -5,6 +5,8 @@ const String SYNCRONIZING_MESSAGE = 'Running data syncronizing';
 const String SYNC_PENDING_MESSAGE = 'Data will sync when internet available';
 const String ONLINE_MESSAGE = 'Online mode';
 const String OFFLINE_MESSAGE = 'No internet connection, running in offline mode';
+const String FIRST_TIME_INTERNET_ERROR_MESSAGE =
+    'No Internet connection! Internet connection is required for the first time app open or user login';
 
 const int MIN_SYNC_INTERVAL_TOLERANCE_FOR_CRITICAL_IN_MINUTES = 5;
 const int MIN_SYNC_INTERVAL_TOLERANCE_FOR_LESS_CRITICAL_IN_MINUTES = 100;
