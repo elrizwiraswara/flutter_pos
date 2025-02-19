01862a2c296898f989c6f1e241a2532aaa62021c
Elriz Wiraswara <elrizwiraswara@gmail.com>
2025-02-20 01:06:50 +0700

    refactor: update FirebaseStorageService to support dependency injection

--- lib/app/services/firebase_storage/firebase_storage_service.dart
+++ lib/app/services/firebase_storage/firebase_storage_service.dart
@@ -1,38 +1,33 @@
 import 'dart:io';
 
-import 'package:firebase_storage/firebase_storage.dart' as storage;
+import 'package:firebase_storage/firebase_storage.dart';
 
 class FirebaseStorageService {
+  final FirebaseStorage firebaseStorage;
+
+  FirebaseStorageService({FirebaseStorage? firebaseStorage})
+      : firebaseStorage = firebaseStorage ?? FirebaseStorage.instance;
+
   Future<String> uploadUserPhoto(String imgPath) async {
-    storage.TaskSnapshot taskSnapshot;
+    final ref = firebaseStorage.ref().child('user_photos').child(
+          'UserImage_${DateTime.now().millisecondsSinceEpoch}.jpg',
+        );
 
-    storage.Reference ref = storage.FirebaseStorage.instance
-        .ref()
-        .child('user_photos')
-        .child('UserImage_${DateTime.now().millisecondsSinceEpoch}.jpg');
+    final metadata = SettableMetadata(contentType: 'image/jpeg');
 
-    final metadata = storage.SettableMetadata(
-      contentType: 'image/jpeg',
-    );
-
-    taskSnapshot = await ref.putFile(File(imgPath), metadata);
+    final taskSnapshot = await ref.putFile(File(imgPath), metadata);
 
     return await taskSnapshot.ref.getDownloadURL();
   }
 
   Future<String> uploadProductImage(String imgPath) async {
-    storage.TaskSnapshot taskSnapshot;
+    final ref = firebaseStorage.ref().child('products').child(
+          'ProductImage_${DateTime.now().millisecondsSinceEpoch}.jpg',
+        );
 
-    storage.Reference ref = storage.FirebaseStorage.instance
-        .ref()
-        .child('products')
-        .child('ProductImage_${DateTime.now().millisecondsSinceEpoch}.jpg');
+    final metadata = SettableMetadata(contentType: 'image/jpeg');
 
-    final metadata = storage.SettableMetadata(
-      contentType: 'image/jpeg',
-    );
-
-    taskSnapshot = await ref.putFile(File(imgPath), metadata);
+    final taskSnapshot = await ref.putFile(File(imgPath), metadata);
 
     return await taskSnapshot.ref.getDownloadURL();
   }
