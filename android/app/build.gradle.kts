a82bd97efce594e016dafdad2303e316a9822931
elrizwiraswara <elrizwiraswara@gmail.com>
2025-10-31 05:12:07 +0700

    update version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.elriztechnology.flutter_pos"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "2.0.0"
         multiDexEnabled = true
     }
 
